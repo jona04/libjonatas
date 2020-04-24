34b9245cb3685b20fe4077c5b44e08cab8dd1f52
jonatasilva <jonatas.iw@gmail.com>
2020-04-24 15:40:28 -0300

    Configurando Setup
    close #6

--- setup.py
+++ setup.py
@@ -63,9 +63,8 @@ def find_package_data(
                             or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print(sys.stderr, (
-                                    "Directory %s ignored by pattern %s"
-                                    % (fn, pattern)))
+                            print("Directory %s ignored by pattern %s" %
+                                  (fn, pattern), file=sys.stderr)
                         break
                 if bad_name:
                     continue
@@ -86,9 +85,8 @@ def find_package_data(
                             or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print(sys.stderr, (
-                                    "File %s ignored by pattern %s"
-                                    % (fn, pattern)))
+                            print("File %s ignored by pattern %s" %
+                                  (fn, pattern), file=sys.stderr)
                         break
                 if bad_name:
                     continue
@@ -122,6 +120,7 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3.8",
         "Framework :: Pytest",
     ],
